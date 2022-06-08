7b933c67f33fb769087552e13869936a858b83a9
lcc-666 <2502247952@qq.com>
2022-06-08 09:02:03 +0800

    简单的优化

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from down.win import down
 开始文件
 """
 if __name__ == '__main__':
-    # url:youtube视频链接
+    # url:B站视频
     Url = "https://www.bilibili.com/video/BV1aY4y1B74o?spm_id_from=333.851.b_7265636f6d6d656e64.1"
     # Path:存储目录，最好是空的
     Path = ""
