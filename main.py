243bf33929abe3cd78c051e185c320b337097142
lcc-666 <2502247952@qq.com>
2022-06-22 17:42:10 +0800

    简单优化

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from down.win import down
 """
 if __name__ == '__main__':
     # url:B站视频
-    Url=input("请输入视频网址\n")
+    Url = input("请输入视频网址\n")
     # Path:存储目录，最好是空的
     Path = ""
     ffmpeg = r"E:\ffmpeg\bin\ffmpeg.exe"
-    Type="mp3"
+    Type = "mp3"
     detail_inputs = {
         "url": Url,
         "path": Path,
