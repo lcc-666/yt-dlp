1f3b22de1c2b0f475f5afc37cef9a4524f105bf0
lcc-666 <2502247952@qq.com>
2022-07-17 17:05:01 +0800

    简单优化

--- down/win.py
+++ down/win.py
@@ -1,15 +1,11 @@
 import os
 import sys
 import yt_dlp
-import shutil
 from yt_dlp import YoutubeDL
-from merge.m4a.mp4 import tomp3
-
-import yt_dlp.postprocessor.ffmpeg
 
 
 def down(detail: dict):
-    if "pornhub" in detail["url"]:
+    if "pornhub" in detail["url"][0]:
         pornhub_down(detail)
     else:
         biliili_down(detail)
@@ -28,56 +24,32 @@ def biliili_down(detail: dict):
         'format': 'bestaudio',
         "outtmpl": path + '%(title)s.%(ext)s'
     }
-    down_type = {"mp4": None, "mp3": ydl_opts}
 
     if detail["type"] is "mp4":
-        ydl_opts["format"]=None
+        ydl_opts["format"] = None
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download(URLS)
     else:
-        ydl_opts["outtmpl"]=path + '%(title)s.mp3'
+        ydl_opts["outtmpl"] = path + '%(title)s.mp3'
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download(URLS)
-            # for item in URLS:
-            #     result = ydl.extract_info(item)
-            #     item_dict = result
-            #     title = item_dict["title"]
-            #     get_type = item_dict["ext"]
-            #     old_name = path + title + " " + "[{}]".format(item_dict["id"]) + "." + get_type
-            #     new_name = path + title + "." + detail["type"]
-            #     os.rename(old_name, new_name)
 
 
 # P站爬虫
 def pornhub_down(detail: dict):
     """
     视频直接获取
-    音频通过ffmepg转换
     """
     path = os.path.abspath(sys.argv[0]).replace("main.py", "")
-    res = os.path.abspath(sys.argv[0]).replace("main.py", "down\\")
-    url = detail["url"][0]
-    down_type = {"mp4": "best", "mp3": "worst"}
-    item = down_type[detail["type"]]
+    url = detail["url"]
+
     opts = {
-        "format": item,
-        "outtmpl": res + '%(title)s.%(ext)s',
+        "format": "best",
+        "outtmpl": path + '%(title)s.%(ext)s',
         "noplaylist": True
     }
     ydl = yt_dlp.YoutubeDL(opts)
-    # ydl.download([url])
-    result = ydl.extract_info(
-        url,  # 视频链接
-    )
-    title = result["title"]
-    item_type = result['ext']
-    if detail['type'] is "mp3":
-        inputs = {res + title + "." + item_type: None}
-        ffmpeg = detail["ffmpeg"]
-        tomp3(inputs, path, title, ffmpeg)
-        os.remove(res + title + "." + item_type)
-    else:
-        shutil.move(res + title + "." + item_type, path + title + "." + item_type)
+    ydl.download(url)
 
 
 if __name__ == '__main__':
